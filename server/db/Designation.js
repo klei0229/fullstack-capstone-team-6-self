b449917af2f09c0fdbab4967d9cca5821c047e60
Jill Thomas <jmthomas8111@gmail.com>
2022-12-03 13:17:38 -0500

    prettier config and format files

--- server/db/Designation.js
+++ server/db/Designation.js
@@ -1,25 +1,20 @@
-
 const conn = require('./conn');
 const { STRING, UUID, UUIDV4, TEXT, BOOLEAN } = conn.Sequelize;
-
 
 const Designation = conn.define('designation', {
   id: {
     type: UUID,
     primaryKey: true,
-    defaultValue: UUIDV4
+    defaultValue: UUIDV4,
   },
   name: {
     type: STRING,
     allowNull: false,
     validate: {
-      notEmpty: true
+      notEmpty: true,
     },
-    unique: true
-  }
+    unique: true,
+  },
 });
 
-
-
 module.exports = Designation;
-
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../store';
 
-const Home = ()=> {
-  const { auth } = useSelector(state => state);
+const Home = () => {
+  const { auth } = useSelector((state) => state);
   const dispatch = useDispatch();
   return (
     <div>
       <h1>Home</h1>
       <div>
-        Welcome { auth.username }!!
-        <button onClick={()=> dispatch(logout())}>Logout</button>
+        Welcome {auth.username}!!
+        <button onClick={() => dispatch(logout())}>Logout</button>
       </div>
     </div>
   );

36c69aabb813016bf53b06f37828dcf6f909bd41
root <root@DESKTOP-9670AL5.localdomain>
2022-12-03 20:59:22 -0500

    implemented csv upload button and setCsvData dispatch method in store.js

---
+++ src/store/csvData.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const csvData = (state = {}, action) => {
+  if (action.type === "SET_CSV_DATA") {
+    return action.csvData;
+  }
+
+  return state;
+};
+
+export const setCsvData = (data) => {
+  return async (dispatch) => {
+    dispatch({ type: "SET_CSV_DATA", csvData: data });
+  };
+};
+
+
+export default csvData;
